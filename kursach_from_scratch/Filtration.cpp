5510ab7600b635ead16f7b750f17d17a5ec2c0d5
krutilins <krutilin.sergey.ks@gmail.com>
2020-04-22 19:26:03 +0300

    add house filtration

--- kursach_from_scratch/Filtration.cpp
+++ kursach_from_scratch/Filtration.cpp
@@ -135,3 +135,37 @@ void filtrStreet(doubly_linked_list* L)
 	}
 	clearList(tempList);
 }
+
+void filtrHouse(doubly_linked_list* L)
+{
+	int start;
+	int end;
+	system("cls");
+	cout << "Введите начало диапазона: " << endl;
+	cin >> start;
+	cout << "Введите конец диапазона: " << endl;
+	cin >> end;
+
+	doubly_linked_list* tempList = (doubly_linked_list*)malloc(sizeof(doubly_linked_list));
+	initialize(tempList);
+
+	Contact* tmp;
+	tmp = L->start->next_link;
+
+	while (tmp != L->end) {
+		if (tmp->house >= start && tmp->house <= end) {
+			endPtr(tempList);
+			prevPut(tempList, tmp);
+		}
+		tmp = tmp->next_link;
+	}
+	if (isEmptyList(tempList)) {
+		cout << "Ничего не найдено" << endl;
+		_getch();
+		system("cls");
+	}
+	else {
+		table(tempList);
+	}
+	clearList(tempList);
+}
