00cad878d0db43c04516c57512ff887921522aff
krutilins <krutilin.sergey.ks@gmail.com>
2020-04-22 19:18:37 +0300

    add phone filtration

--- kursach_from_scratch/Filtration.cpp
+++ kursach_from_scratch/Filtration.cpp
@@ -33,3 +33,37 @@ void filtrFIO(doubly_linked_list* L)
 	}
 	clearList(tempList);
 }
+
+void filtrPhone(doubly_linked_list* L)
+{
+	char start;
+	char end;
+	system("cls");
+	cout << "Введите начало диапазона: " << endl;
+	cin >> start;
+	cout << "Введите конец диапазона: " << endl;
+	cin >> end;
+
+	doubly_linked_list* tempList = (doubly_linked_list*)malloc(sizeof(doubly_linked_list));
+	initialize(tempList);
+
+	Contact* tmp;
+	tmp = L->start->next_link;
+
+	while (tmp != L->end) {
+		if (tmp->phone[0] >= start && tmp->phone[0] <= end) {
+			endPtr(tempList);
+			prevPut(tempList, tmp);
+		}
+		tmp = tmp->next_link;
+	}
+	if (isEmptyList(tempList)) {
+		cout << "Ничего не найдено" << endl;
+		_getch();
+		system("cls");
+	}
+	else {
+		table(tempList);
+	}
+	clearList(tempList);
+}
